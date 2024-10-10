7ee0faba2761794fcf368e3d00816090d6fc2d8a
jephthe <jephthepetitfrere@gmail.com>
2024-10-10 17:46:03 -0400

    installed helmet & compression

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
 
 // APP + Configurations
 dotenv.config();
@@ -14,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 // Mongoose connection event listener
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
 });
 mongoose.connection.on('error', (err) => {
   console.log('Mongoose connection error:', err);
@@ -27,6 +29,11 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev')); // Morgan middleware to log requests
+app.use(helmet()); // Secure the app by setting various HTTP headers
+app.use(compression()); // Gzip all responses for faster load time
+
+// Static Files (CSS, Images, etc.)
+app.use(express.static('public')); 
 
 // Landing Page - Home page
 app.get('/', (req, res) => {
@@ -37,5 +44,11 @@ app.get('/', (req, res) => {
 const teamRoutes = require('./routes/teamRoutes');
 app.use('/teams', teamRoutes);
 
-// Export app
+// Error-handling middleware for generic server-side errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke! Our team is working on it.');
+});
+
+// Export the app for server.js
 module.exports = app;
